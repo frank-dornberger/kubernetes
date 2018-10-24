3c020b15a629fa5db77aefe220a7f5935d29b2d0
frank-dornberger <frank@dornberger.ch>
2018-10-25 00:11:25 +0200

    Add pseudo code for Key Transaction configuration

--- apm.tf
+++ apm.tf
@@ -5,4 +5,13 @@
 #   end_user_apdex_threshold    = 0.8
 #   enable_real_user_monitoring = true
 # }
+# data "newrelic_key_transaction" "txn" {
+#   name = "key_transaction"
+# }
+## Needs API exposure
+# resource "newrelic_key_transaction_setting" "hello_world" {
+#   entity                      = "${data.newrelic_key_transaction.txn.id}"
+#   app_apdex_threshold         = 0.9
+#   end_user_apdex_threshold    = 0.9
+# }
 
