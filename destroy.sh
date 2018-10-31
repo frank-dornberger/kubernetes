8d27f7a04d93fcc8d2e7a2a25b4226e6325a9a33
frank-dornberger <frank@dornberger.ch>
2018-10-31 01:03:16 +0000

    Also remove env variables on destruction

--- destroy.sh
+++ destroy.sh
@@ -2,5 +2,7 @@
 kubectl delete -f hello-world-deployment.yaml --kubeconfig ./kubeconfig_frank-cluster-1
 echo "hello-world app removed"
 terraform destroy -auto-approve
-unset KUBECONFIG
 echo "Infrastructure and monitoring removed"
+rm .env
+unset KUBECONFIG
+echo "ENV variables unset"
